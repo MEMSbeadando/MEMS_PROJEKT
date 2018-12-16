8e50593fac0c4adbffc44edac438afbcb0e83263
MEMSbeadando <44005617+MEMSbeadando@users.noreply.github.com>
2018-12-16 21:06:33 +0100

    Update adatkeres2.py

--- adatkeres2.py
+++ adatkeres2.py
@@ -5,10 +5,17 @@ from datetime import datetime
 import sys #valtozokhoz hozzaferes
 from csv import writer #modulbeimportalas
 import csv
+import email_kuldes
+import parameterek#allithato meresi parameterek
 
 
 sense = SenseHat() # kapcsolat a senshattel
 sense.clear() #minden ledet takarit
+ido1=parameterek.elsoemail#elso email kuldesenek ideje
+ido2=parameterek.masodikemail
+utolsoemail= 25
+idokoz=parameterek.idokoz#meresek kozt eltelt ido masodpercben
+meresenkent=parameterek.meresenkent#meresenkent kuldjon-e uzenetet?
 
 try:
       while True:
@@ -37,6 +44,14 @@ try:
            sense.show_message("Hom.: " + str(temp) + " C " + "Parat.:" + str(humidity) + " % " + "Legny.:" + str(pressure) + " hPa"
                               + "Ido: " + str(hour), scroll_speed=(0.08), back_colour= [0,0,0], text_colour= [187,10,48],)
 
-           time.sleep(30) # mennyi idokozonkent fusson a kod
+           akt_ora=datetime.now().hour#Aktualis ora
+
+           if (akt_ora==ido1 or akt_ora==ido2 or meresenkent) and (utolsoemail!=akt_ora or meresenkent):#Ne kuldjon e-mailt minden loopnal egy oran belul
+                email_kuldes.emailkuldes(temp,humidity,pressure)
+                utolsoemail=akt_ora
+
+
+
+           time.sleep(idokoz) # mennyi idokozonkent fusson a kod
 except KeyboardInterrupt:
       pass
